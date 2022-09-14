9964a886f35b98f3ba7020fe94b05baa465c78a3
dante <974668431@qq.com>
2022-09-14 23:14:55 +0800

    first

--- 0-basic/json.py
+++ 0-basic/json.py
@@ -1,6 +1,6 @@
 # 读
 import json
-with open('../8-ner_nre/pytorch-master/train_data_me.json', 'r') as file:
+with open('../8-ner_nre/pytorch_nn_rnn-master/train_data_me.json', 'r') as file:
     str = file.read()
     data = json.loads(str)
     for i in data:
